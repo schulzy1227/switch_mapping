63d1b89004479b4411576902ba64cd228642f1b0
schulzy1227 <123996943+schulzy1227@users.noreply.github.com>
2023-02-27 14:21:26 -0600

    two dataframes. one for key pairs of switch file and other key pairs of sitehealth csv.

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+data_one = pd.read_csv('C:\\Switch_Charts\\Switch_50.csv')
+
+name_ip_list = []
+ip_port_list = []
+
+for index, row in data_one.iterrows():
+    ip_add = row[0]
+    switch_port = row[5]
+    port_ip_pair = {ip_add: switch_port}
+    ip_port_list.append(port_ip_pair)
+
+
+site_health = pd.read_csv('C:\\data_pull_downloads\\IslandView.csv', skiprows=198)
+for index, row in site_health.iterrows():
+    device_name = row[1]
+    ip = row[8]
+    dev_ip_pair = {ip: device_name}
+    name_ip_list.append(dev_ip_pair)
+
+for each in name_ip_list:
+    for k, v in each.items():
+        print(k, v)
+
+# loop through selecting keys and values
+# for each in ip_port_list:
+#     for k, v in each.items():
+#         # if value is null, replace with  X
+#         if pd.isna(v):
+#             v = 'x'
+#     print(f'Key: {k}, Value:{v}')
